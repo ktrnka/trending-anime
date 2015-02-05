ae78b131b6edec9825c8b8ffa50b28081903006f
Keith Trnka <keith.trnka@gmail.com>
2015-02-04 22:58:12 -0800

    Fixing silliness

--- list_episode_info.py
+++ list_episode_info.py
@@ -1,7 +1,9 @@
 import ConfigParser
 import argparse
 import logging
+from pprint import pprint
 import sys
+import datetime
 import pymongo
 
 __author__ = 'keith'
@@ -20,7 +22,19 @@ def main():
     config.read([args.config])
 
     mongo_client = pymongo.MongoClient(config.get("mongo", "uri"))
-    collection = mongo_client["anime-trends"]
+    collection = mongo_client.get_default_database()["animes"]
+
+    for anime in collection.find():
+        print anime["key"]
+
+        episodes = [(int(ep), v) for ep, v in anime.get("download_history", {}).iteritems()]
+        episodes = sorted(episodes, key=lambda p: p[0])
+
+        for episode, download_counts in episodes:
+            print "Episode {}".format(episode)
+            download_counts = {datetime.datetime.strptime(k, make_site.MONGO_TIME):int(v) for k, v in download_counts.iteritems()}
+            for date, downloads in sorted(download_counts.iteritems(), key=lambda p: p[0]):
+                print "\t{}: {:,}".format(date.strftime("%Y-%m-%d"), downloads)
 
 
 
