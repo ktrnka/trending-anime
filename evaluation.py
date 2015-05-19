6b10a3eb37d9d37c9f77264cc85b8aa96528d9db
Keith Trnka <keith.trnka@gmail.com>
2015-05-19 13:17:25 -0700

    Cleaning up a tiny bit

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+import logging
 import sys
 import argparse
 import math
@@ -8,13 +9,17 @@ class InsufficientDataError(ValueError):
     pass
 
 class Evaluation(object):
-    def __init__(self):
-        self.max_days_training = 4
-        self.predict_days_range = (7, 8)
+    def __init__(self, x_max):
+        self.logger = logging.getLogger(__name__)
+        self.x_max = x_max
+        self.testing_x_range = (7, 8)
+
+        if self.x_max >= self.testing_x_range[0]:
+            self.logger.warning("Testing x range overlaps training range: {} vs {}".format(self.x_max, self.testing_x_range))
 
     def evaluate(self, model, datapoints):
-        training_data = [p for p in datapoints if p[0] < self.max_days_training]
-        testing_data = [p for p in datapoints if p[0] >= self.predict_days_range[0] and p[0] <= self.predict_days_range[1]]
+        training_data = [p for p in datapoints if p[0] < self.x_max]
+        testing_data = [p for p in datapoints if p[0] >= self.testing_x_range[0] and p[0] <= self.testing_x_range[1]]
 
         if not training_data:
             raise InsufficientDataError()
@@ -29,9 +34,9 @@ class Evaluation(object):
     def score(self, reference_y, predicted_y):
         errors = []
         for ref, pred in zip(reference_y, predicted_y):
-            errors.append(math.abs(pred - ref) / ref)
+            errors.append(abs(pred - ref) / float(ref))
 
-        return sum(errors) / float(len(errors))
+        return sum(errors) / len(errors)
 
 
 def parse_args():
