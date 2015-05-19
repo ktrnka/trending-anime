925708846a3aff95f94165665415b1f968c4a7cc
Keith Trnka <keith.trnka@gmail.com>
2015-05-19 13:12:49 -0700

    Changing to explore the prediction quality

--- list_episode_info.py
+++ list_episode_info.py
@@ -1,13 +1,11 @@
 import ConfigParser
 import argparse
-import collections
-import io
-import json
 import logging
-from pprint import pprint
 import sys
-import datetime
+
 import pymongo
+import download_graph
+
 
 __author__ = 'keith'
 
@@ -43,7 +41,7 @@ def main():
     scores_new = dict()
 
     for anime in collection.find():
-        if "ghoul" not in anime["key"].lower():
+        if "seraph" not in anime["key"].lower():
             continue
 
         series = make_site.Series()
@@ -57,32 +55,20 @@ def main():
 
         episodes = sorted(series.episodes.iterkeys())
 
-        downloads_old = series.estimate_downloads_old()
-        downloads_new = series.estimate_downloads(7)
-
-        scores_old[series.url] = mean(downloads_old.values())
-        print "Old value: {}".format(mean(downloads_old.values()))
-
-        try:
-            scores_new[series.url] = make_site.PredictedValue.weighted_average(downloads_new.values())
-        except ZeroDivisionError:
-            scores_new[series.url] = -1
-        print "New value: {:.1f}".format(scores_new[series.url])
+        predictions = series.estimate_downloads_debug(7, 4)
 
         for episode in episodes:
             release_date = series.episodes[episode].get_release_date()
             print "Episode {}: {}".format(episode, release_date.strftime("%Y-%m-%d"))
-            print "\tDownloads (old): {}".format(downloads_old.get(episode, -1))
-            print "\tDownloads (new): {}".format(downloads_new.get(episode, -1))
+
+            reference_data, prediction_data = predictions[episode]
+            reference_x, reference_y = reference_data
+            pred_x, pred_y = prediction_data
+            download_graph.make_downloads_graph(zip(reference_x, reference_y), "temp/{}.png".format(episode), prediction_data=zip(pred_x, pred_y))
 
             for date, downloads in sorted(series.episodes[episode].downloads_history.iteritems(), key=lambda p: p[0]):
                 print "\t{}: {:,}".format(date.strftime("%Y-%m-%d %H:%M"), downloads)
 
-    for scores in [scores_old, scores_new]:
-        print "Scores:"
-        sorted_pairs = sorted(scores.iteritems(), key=lambda p: p[1], reverse=True)
-        for pair in sorted_pairs[:20]:
-            print "\t{}: {:,}".format(pair[0], int(pair[1]))
 
 
 
