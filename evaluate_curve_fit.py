1bfa9b9bdbb685fe886659fe1958713c238f3fe7
Keith Trnka <keith.trnka@gmail.com>
2015-05-21 12:57:44 -0700

    Adding a few more functions (none with backoff) and making a learning-like curve

--- evaluate_curve_fit.py
+++ evaluate_curve_fit.py
@@ -14,12 +14,12 @@ import pymongo
 import download_graph
 import matplotlib.pyplot as plt
 
-
 import make_site
 import curve_fitting
 
 
 FILENAME_CHARS = set("-_.() " + string.ascii_letters + string.digits)
+
 
 def mean(values):
     if not values:
@@ -29,6 +29,29 @@ def mean(values):
 
 def clean_filename(s):
     return "".join(c for c in s if c in FILENAME_CHARS)
+
+
+def learning_curve(data, filename):
+    plt.figure()
+    plt.title("Prediction accuracy vs number of days training data")
+    plt.xlabel("Number of days of training data available")
+    plt.ylabel("Fraction difference from actual values")
+
+    plt.ylim((0., 1.))
+
+    plt.grid()
+
+    for data_name in data.iterkeys():
+        x_values = sorted(data[data_name].iterkeys())
+        y_values = [data[data_name][x].mean() for x in x_values]
+
+        plt.plot(x_values, y_values, "o-", label=data_name)
+
+    plt.legend(loc="best")
+
+    plt.savefig(filename, bbox_inches="tight")
+    plt.close()
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -45,19 +68,34 @@ def main():
 
     backoff_zero = curve_fitting.ConstantCurve(lambda x: 0, "Zero", "0")
 
-    simplest_log = curve_fitting.SimpleLogCurve()
+    # simplest_log = curve_fitting.SimpleLogCurve()
 
-    log_curve = curve_fitting.Curve(lambda x, a, b, c: b * numpy.power(numpy.log(x + a + 1), c), "log3", "{1} * (log(x + {0} + 1) ^ {2}",
-                      backoff_curve=backoff_zero)
-    log_curve_simple = curve_fitting.Curve(lambda x, b: b * numpy.power(numpy.log(x + 1), 0.5), "log1", "{0} * log(x + 1) ^ 0.5",
-                             backoff_curve=simplest_log)
+    log_curve_3 = curve_fitting.Curve(lambda x, a, b, c: b * numpy.power(numpy.log(x + a + 1), c), "log 3p",
+                                      "{1} * (log(x + {0} + 1) ^ {2}",
+                                      backoff_curve=backoff_zero)
 
-    log_curve_plus = curve_fitting.Curve(lambda x, a, b, c: b * numpy.power(numpy.log(x + a + 1), c), "log3+1", "{1} * (log(x + {0} + 1) ^ {2}",
-                      backoff_curve=log_curve_simple, min_points=4)
+    log_curve_2exp = curve_fitting.Curve(lambda x, a, b: b * numpy.power(numpy.log(x + 1), a), "log 2p_exp",
+                                      "{1} * (log(x + 1) ^ {0}",
+                                      backoff_curve=backoff_zero)
 
-    inverse_curve = curve_fitting.Curve(lambda x, a, b: x / (x + a ** 2) * b, "inv", "x / (x + {0}^2) * {1}", backoff_curve=log_curve_simple, min_points=3)
+    log_curve_2shift = curve_fitting.Curve(lambda x, a, b: b * numpy.power(numpy.log(x + a + 1), 0.5), "log 2p_shift",
+                                      "{1} * (log(x + {0} + 1) ^ 0.5",
+                                      backoff_curve=backoff_zero)
 
-    evaluation_suite = curve_fitting.EvaluationSuite([curve_fitting.Evaluation(2), curve_fitting.Evaluation(3), curve_fitting.Evaluation(4), curve_fitting.Evaluation(5)], [log_curve, log_curve_simple, log_curve_plus, inverse_curve])
+    log_curve_1 = curve_fitting.Curve(lambda x, b: b * numpy.power(numpy.log(x + 1), 0.5), "log 1p",
+                                      "{0} * log(x + 1) ^ 0.5",
+                                      backoff_curve=backoff_zero)
+
+    # log_curve_plus = curve_fitting.Curve(lambda x, a, b, c: b * numpy.power(numpy.log(x + a + 1), c), "log3+1", "{1} * (log(x + {0} + 1) ^ {2}",
+    # backoff_curve=log_curve_simple, min_points=4)
+
+    inverse_curve = curve_fitting.Curve(lambda x, a, b: x / (x + a ** 2) * b, "inv 2p", "x / (x + {0}^2) * {1}",
+                                        backoff_curve=backoff_zero, min_points=3)
+
+    evaluation_suite = curve_fitting.EvaluationSuite(
+        [curve_fitting.Evaluation(1), curve_fitting.Evaluation(2), curve_fitting.Evaluation(3),
+         curve_fitting.Evaluation(4), curve_fitting.Evaluation(5), curve_fitting.Evaluation(6),
+         curve_fitting.Evaluation(7), curve_fitting.Evaluation(8)], [log_curve_1, log_curve_2exp, log_curve_2shift, log_curve_3, inverse_curve])
 
     scores = collections.defaultdict(lambda: collections.defaultdict(list))
 
@@ -97,7 +135,7 @@ def main():
             try:
                 all_scores = []
                 for evaluation, model, score in evaluation_suite.evaluate(datapoints):
-                    scores[evaluation.x_max][model.name].append(score)
+                    scores[model.name][evaluation.x_max].append(score)
                     all_scores.append(score)
 
                     if score > 0.5 and score < 1.0:
@@ -107,34 +145,37 @@ def main():
 
                 if all(s > 0.4 for s in all_scores):
                     series_part = clean_filename(series.url.split("/")[-1])
-                    download_graph.make_downloads_graph(datapoints, "unusual_downloads/{}_{}.png".format(series_part, episode))
+                    download_graph.make_downloads_graph(datapoints,
+                                                        "unusual_downloads/{}_{}.png".format(series_part, episode))
 
                 if all(s < 0.25 for s in all_scores):
                     series_part = clean_filename(series.url.split("/")[-1])
-                    download_graph.make_downloads_graph(datapoints, "normal_downloads/{}_{}.png".format(series_part, episode))
+                    download_graph.make_downloads_graph(datapoints,
+                                                        "normal_downloads/{}_{}.png".format(series_part, episode))
 
             except curve_fitting.InsufficientDataError:
                 pass
 
-    for evaluation, scored_models in scores.iteritems():
-        print "Summary at evaluation xmax {}".format(evaluation)
+    for model_name, scored_models in scores.items():
+        scores[model_name] = {x: pandas.Series(scores) for x, scores in scored_models.iteritems()}
 
-        scored_models = {model: pandas.Series(scores) for model, scores in scored_models.iteritems()}
-        for model_name, scores in scored_models.iteritems():
-            print "\t{}: {:.3f} +/- {:.3f} in {:,} tests".format(model_name, scores.mean(), scores.std(), scores.shape[0])
+    learning_curve(scores, "learning_curve.png")
+
+    for model_name, scored_models in scores.iteritems():
+        print "Model {}".format(model_name)
+
+        for evaluation, scores in scored_models.iteritems():
+            print "\t{}: {:.3f} +/- {:.3f} in {:,} tests".format(evaluation, scores.mean(), scores.std(),
+                                                                 scores.shape[0])
 
             nondefault = scores[scores < 0.99]
-            print "\t\tnondefault: {:.3f} +/- {:.3f} in {:,} tests".format(nondefault.mean(), nondefault.std(), nondefault.shape[0])
+            print "\t\tnondefault: {:.3f} +/- {:.3f} in {:,} tests".format(nondefault.mean(), nondefault.std(),
+                                                                           nondefault.shape[0])
 
             plt.clf()
             scores.hist(bins=25)
             plt.savefig("model_scores_{}_{}.png".format(evaluation, model_name))
             plt.close()
-
-
-
-
-
 
 
 if __name__ == "__main__":
