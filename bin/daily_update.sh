d37200d77ba982ce0e1e1e27a455c987e50c41e5
Keith Trnka <keith.trnka@gmail.com>
2015-07-01 15:19:35 -0700

    Ooops including CSS resources in the command so it knows to inline them.

--- bin/daily_update.sh
+++ bin/daily_update.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python make_old_site.py -v config 2015/04/01 bitballoon/winter2015.html
-python make_site.py -v config bitballoon
+python make_old_site.py -v config 2015/04/01 bitballoon/winter2015.html res/extra-fonts.css res/fonts res/about.html
+python make_site.py -v config bitballoon res/extra-fonts.css res/fonts res/about.html
