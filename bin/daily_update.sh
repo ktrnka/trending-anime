1bed3295bad24223ef995e625f55348c3854aa82
Keith Trnka <keith.trnka@gmail.com>
2015-11-13 14:57:07 -0800

    Fixing issue where the font icons weren't being linked in the html

--- bin/daily_update.sh
+++ bin/daily_update.sh
@@ -3,8 +3,8 @@ set -x
 
 rm -rf site
 mkdir -p site
-python make_old_site.py -v config 2015/04/01 site/winter2015.html
-python make_old_site.py -v config 2015/07/01 site/spring2015.html
-python make_old_site.py -v config 2015/10/01 site/summer2015.html
+python make_old_site.py -v config 2015/04/01 site/winter2015.html res/extra-fonts.css res/fonts
+python make_old_site.py -v config 2015/07/01 site/spring2015.html res/extra-fonts.css res/fonts
+python make_old_site.py -v config 2015/10/01 site/summer2015.html res/extra-fonts.css res/fonts
 python make_site.py -v config site/index.html res/extra-fonts.css res/fonts res/about.html
 python deploy_site.py -v config site
